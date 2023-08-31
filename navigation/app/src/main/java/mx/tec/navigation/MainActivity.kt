df18cf1764e0f4c8088a5e164c32a3f79bca0c2e
Juan Zambrano <jpablozambrano@hotmail.com>
2023-08-31 08:45:51 -0600

    Navigation example

--- navigation/app/src/main/java/mx/tec/navigation/MainActivity.kt
+++ navigation/app/src/main/java/mx/tec/navigation/MainActivity.kt
@@ -3,13 +3,21 @@ package mx.tec.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import mx.tec.navigation.ui.theme.NavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NavigationExample()
                 }
             }
         }
@@ -30,17 +38,107 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun NavigationExample(){
+    // this will be the main manager for our views
+    // first we need to declare a controller
+    // the controller is the object in charge of changing the views
+    // and data exchange
+    val navController = rememberNavController()
+
+    // a host is a structure in which several interfaces live
+    NavHost(
+        navController = navController,
+        startDestination = "mainMenu"
+    ){
+        // within the navhost we are going to declare several composables to navigate
+        // using the composable macro
+        composable("mainMenu"){
+            MainMenu(
+                kittenInterfaceButtonLogic = {
+                    navController.navigate("KittenInterface")
+                },
+                puppyInterfaceButtonLogic = {
+                    navController.navigate("PuppyInterface")
+                }
+            )
+        }
+        composable("KittenInterface"){
+            KittenInterface(
+                goBack = {
+                    navController.popBackStack()
+                }
+            )}
+        composable("PuppyInterface"){
+            PuppyInterface(
+                goBack = {
+                    navController.popBackStack()
+                }
+        )}
+    }
+
 }
 
+@Composable
+fun MainMenu(
+    kittenInterfaceButtonLogic : () -> Unit,
+    puppyInterfaceButtonLogic : () -> Unit
+){
+    Column (horizontalAlignment = Alignment.CenterHorizontally){
+        Button(
+            // No brackets
+            // with brackets we declare a function
+            onClick = kittenInterfaceButtonLogic
+        ) {
+            Text("Go to kitten interface")
+        }
+        Button(
+            onClick = puppyInterfaceButtonLogic
+
+        ) {
+            Text("Go to puppy interface")
+        }
+    }
+}
+
+@Composable
+fun KittenInterface(
+    goBack : () -> Unit
+) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        AsyncImage(model = "https://www.warrenphotographic.co.uk/photography/sqrs/14819.jpg",
+            contentDescription = "A kitten"
+        )
+        Button(
+            onClick = goBack
+        ) {
+            Text("Go back")
+        }
+    }
+}
+
+@Composable
+fun PuppyInterface(
+    goBack : () -> Unit
+) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        AsyncImage(model = "https://www.warrenphotographic.co.uk/photography/sqrs/41644.jpg",
+            contentDescription = "A puppy"
+        )
+        Button(
+            onClick = goBack
+        ) {
+            Text("Go back")
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun NavigationExamplePreview() {
     NavigationTheme {
-        Greeting("Android")
+        NavigationExample()
     }
 }
