44b4c5627799a6f17e473f65a73b4ba7ab27183b
Juan Zambrano <jpablozambrano@hotmail.com>
2023-09-07 13:13:09 -0600

    finished Menu activity

--- Act33/app/src/main/java/mx/tec/act33/MenuActivity.kt
+++ Act33/app/src/main/java/mx/tec/act33/MenuActivity.kt
@@ -12,20 +12,29 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import mx.tec.act33.ui.theme.Act33Theme
@@ -86,6 +95,11 @@ class MenuActivity : ComponentActivity() {
                 val name = animal.name
                 val age = animal.age
                 val weight = animal.weight
+                ExpandableCard(
+                    nameEC = name,
+                    bodyEC = "Age: $age, weight: $weight"
+                )
+                /*
                 Row {
                     Text(text = name)
                     Spacer(modifier = Modifier.width(8.dp))
@@ -98,26 +112,53 @@ class MenuActivity : ComponentActivity() {
                     }) {
                         Text(text = "Show Details")
                     }
-                }
+                }*/
             }
             Button(onClick = {
-                LoadDetailActivity(this)   
+                loadDetailActivity(this)
             }) {
-                Text(text = "Create a new animal")   
+                Text(text = "Create a new animal")
             }
-            
         }
     }
 
-    fun LoadDetailActivity(view: ColumnScope){
+    private fun loadDetailActivity(view: ColumnScope){
         val intent = Intent(this, DetailActivity::class.java)
         startActivity(intent)
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ExpandableCard(nameEC: String?, bodyEC: String?) {
+        var expanded by remember { mutableStateOf(false) }
+        ElevatedCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            onClick = { expanded = !expanded}
+        ) {
+            Text(
+                text = "Name: $nameEC",
+                modifier = Modifier
+                    .padding(16.dp),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            if(expanded){
+                Text(
+                    text = "Additional info: $bodyEC",
+                    modifier = Modifier.padding(16.dp),
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
     }
     @Preview(showBackground = true)
     @Composable
     fun GreetingPreview2() {
         Act33Theme {
             LoadAnimals()
+            ExpandableCard(nameEC = "hola", bodyEC = "ff")
         }
     }
 }
